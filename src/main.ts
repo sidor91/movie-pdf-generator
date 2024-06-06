4f8b0ac854010750d3d2f301c16cb37051a70e27
sidor91 <sidorchik91@gmail.com>
2024-06-06 21:37:24 +0200

    ADD | app-config; EDIT | main and app.controller

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('Starting Movies PDF Generator server... 🚀🚀🚀 ');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
