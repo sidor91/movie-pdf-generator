3633277b8c9a98b2cd31cd8224259fa38dc5484c
sidor91 <sidorchik91@gmail.com>
2024-06-06 21:46:16 +0200

    UPDATE | types

--- src/movies/types/index.ts
+++ src/movies/types/index.ts
@@ -1,13 +1,13 @@
-export type AllMoviesResultType = {
-  link: string;
+type CommonResultsType = {
   title: string;
   release_date: string;
   vote_average: number;
 };
 
-export type MovieByIdResultType = {
-  title: string;
-  release_date: string;
-  vote_average: number;
+export type AllMoviesResultType = CommonResultsType & {
+  link: string;
+};
+
+export type MovieByIdResultType = CommonResultsType & {
   poster_path: string;
 };
